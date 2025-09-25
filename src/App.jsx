c22558fe721dc00e0f7c697309e6422b4eca7c37
gecm-2022 <harshprajapati07ce@gmail.com>
2025-09-25 19:06:51 +0530

    add

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import { useState, useEffect } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import axios from 'axios'
+import { useQuery } from '@tanstack/react-query'
+import Card from './components/card'
+
+function App() {
+  const [serach, setSearch] = useState("")
+  const [pages, setPages] = useState(4)
+  const [previos, setPrevios] = useState(0)
+
+
+
+
+  const fetchData = async () => {
+
+    const response = await axios.get("https://685d3183769de2bf085fb3e1.mockapi.io/api/v1/pokemons")
+    return response.data;
+
+  }
+
+
+  const { data, isLoading } = useQuery({ queryKey: ['data'], queryFn: fetchData })
+
+
+
+
+  if (isLoading) {
+    return (
+      <div className=" items-center flex text-3xl text-white justify-center h-screen w-full ">
+        loading........
+      </div>
+    )
+
+  }
+  const allData = serach ? data?.filter((e) => (e.name === serach)).slice(0, 4) : data.slice(previos, pages)
+
+
+  const handlePage = (e) => {
+
+    // setPrevios(pages)
+    // setPages(4 * e)
+
+    switch (e) {
+      case 1:
+        setPrevios(0)
+        setPages(4)
+
+        break;
+      case 2:
+        setPrevios(4)
+        setPages(8)
+
+        break;
+      case 3:
+        setPrevios(8)
+        setPages(12)
+
+        break;
+      case 4:
+        setPrevios(12)
+        setPages(16)
+
+        break;
+      case 5:
+        setPrevios(16)
+        setPages(20)
+
+        break;
+      case 6:
+        setPrevios(20)
+        setPages(24)
+
+        break;
+
+      default:
+        break;
+    }
+    // if (e === 1) {
+    //   setPrevios(0)
+    //   setPages(4)
+
+    // }
+  }
+
+
+  return (
+    <div className='bg-black w-full h-screen'>
+      <div className="    flex flex-col items-center    justify-center  ">
+        <div className="  flex flex-col item-center h-fit w-full mx-auto items-center mt-7 mb-3">
+
+          <h1 className='text-blue-800 font-bold items-center  text-4xl  '>Pokédex</h1>
+
+          <p className='text-gray-600'>Discover amazing Pokémon and their incredible super powers
+
+          </p>
+        </div>
+
+        <div className=" flex items-center text-white   my-5 w-1/3 content-center ">
+          <input
+            name='serach'
+            onChange={(e) => setSearch(e.target.value)}
+            type="text" placeholder='Search by name species or type ' className=' p-2 bg-white w-full text-black rounded-2xl' />
+        </div>
+      </div>
+
+      <Card allData={allData} />
+      <div className=" w-full text-2xl text-white items-center  flex justify-center mx-auto m-auto ">
+
+
+        {[1, 2, 3, 4, 5, 6].map((e, i) =>
+        (
+          <div className="flex  gap-3" key={i}>
+
+            <button
+              onClick={() => handlePage(e)} className=" flex mx-2 border px-3 mb-8 rounded-xl py-1 bg-blue-800"  >{e}</button>
+          </div>
+        ))}
+
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+const Card = ({ allData }) => {
+    return (
+        <div className=" w-auto md:flex-row flex-col my-5 flex gap-8 overflow-hidden p-6 mx-10 text-white">
+            {allData?.map((e) => (
+
+                <div key={e.id} className={`flex flex-wrap cursor-pointer  hover:scale-110   `}>
+                    <div
+                        style={{ backgroundColor: e.card_color }}
+                        className={`border border-yellow-300  w-[300px] rounded-2xl items-center  p-4 text-white h-[350px] `}>
+
+                        <div className=" w-[150px] h-fit items-center content-center m-auto  mx-auto flex justify-center ">
+
+                            <img src={e.image} alt="" />
+
+                        </div>
+                        <div className=" flex flex-col items-center my-2">
+
+                            <h1 className="text-gray-300 font-bold">
+                                {e.name}
+                            </h1>
+                            <p className="text-black text-xl font-bold">{e.species}</p>
+                        </div>
+                        <p style={{ backgroundColor: e.species_color }}
+                            className={` rounded-2xl   w-fit p-2 border flex items-center justify-center  m-auto`}>{e.type}</p>
+                        {/* <p>{e.species_color}</p> */}
+
+                        <div className="  w-full my-3">
+                            <h1 className='text-xl text-gray-400 mb-2'>Super Powers :</h1>
+                            <div className=' w-full flex gap-3 flex-wrap '>
+
+
+                                {e?.super_powers?.map((p) => (
+
+                                    <div className="flex  text-sm  p-1 px-2   shadow-inner  text-gray-200 rounded-2xl bg-white font-mono ">
+                                        {p}
+                                    </div>
+
+
+                                ))}
+                            </div>
+
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+
+            ))}
+
+
+        </div>
+    )
+}
+
+export default Card
